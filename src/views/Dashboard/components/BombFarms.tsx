9ff155f4000401dfbcc43523683388d2d13be100
Rishabh Raghav <rishabhraghav2012@gmail.com>
2023-05-08 15:49:29 +0530

    added bombfarms UI

--- src/views/Dashboard/components/BombFarms.tsx
+++ src/views/Dashboard/components/BombFarms.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import useWithdrawFromBomb from "../../../hooks/useWithdrawFromBomb";
+import useStakedBombBalance from '../../../hooks/useStakedBombBalance';
+import { getDisplayBalance } from '../../../utils/formatBalance';
+import useTotalValueLocked from '../../../hooks/useTotalValueLocked';
 
 
 const BombFarms = () => {
+    const {onWithdraw} = useWithdrawFromBomb();
+    const stackedBomb = useStakedBombBalance();
+    const tvl = useTotalValueLocked();
 
+    const handleWithdrawBomb = () => {
+        const amount = getDisplayBalance(stackedBomb);
+        onWithdraw(amount);
+    }
     return (
         <div className="BombFarms marginForAll">
             <div className="headings">
@@ -18,8 +28,8 @@ const BombFarms = () => {
                 <div className="boardroomHeadings" style={{width:"550px"}}>
                     <p className="headlines">BOMB-BTCB <span className="recommended">Recommended</span></p>
                     <div style={{display: "flex", justifyContent:"space-between", flexDirection:"row"}}>
-                        <p className="headlines" style={{fontSize: "smaller"}}>Stake BSHARE and earn BOMB every epoch</p>
-                        <p style={{color:"white"}}>TVL: ${"tvl.toFixed(2)"}</p>
+               
+                        <p style={{color:"white"}}>TVL: ${tvl.toFixed(2)}</p>
                     </div>
                 </div>
             </div>
@@ -46,7 +56,7 @@ const BombFarms = () => {
                     <div>
                         <button className="button boardroomDeposit" >Deposit <img style={{height:"18px", width:"18px", backgroundColor:"#6C9BCF", marginLeft:"3px"}} alt="img" src="https://img.icons8.com/?size=512&id=100000&format=png"/></button>
 
-                        <button className="button boardroomWithdraw" >Withdraw <img style={{height:"20px", width:"20px", backgroundColor:"#6C9BCF", marginLeft:"3px"}} alt="img" src="https://img.icons8.com/?size=512&id=102673&format=png"/></button>
+                        <button className="button boardroomWithdraw" onClick={handleWithdrawBomb}>Withdraw <img style={{height:"20px", width:"20px", backgroundColor:"#6C9BCF", marginLeft:"3px"}} alt="img" src="https://img.icons8.com/?size=512&id=102673&format=png"/></button>
 
                     </div>
                     <button style={{display: "flex", justifyContent: "center", alignItems:"center"}} className="button boardroomClaim" >Claim Rewards <img style={{height:"18px", width:"18px"}} alt="bombimg"  src="https://s3-alpha-sig.figma.com/img/a4ce/8dfa/11770664532158db85f355f7654b42d7?Expires=1684108800&Signature=CIlhPZwuugGXFhdDHFxthynQV6kuCT9o3J9oOSBtBnEFBsVx7dlARHPg9GCRnYniOUwEbORcXUPG02g6mb4Z-LZc8R1POR9lbVgdNnxj3~o~9~80CvFAf6eF6TbnBKT-RqTkl8Iu8a5RPfKcIgIjfDTFACUlphyo-Eq1tX-AZX26RPS9eFClnrRvSZZry97DctNCN~nyu6~8ZtXlgc1WuEPmKF9M0fE5XrciIUNMDDAfKvZ2tKOtOkLpcyXS~jXRJJOEnKtCmA34y1MEhnuF3EHz4PNFhNGZBybNmc4NLlvO-PbratXyv1Qd9VmlnY0E1f3p5u1Zd712XXgZeCPYAw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"  /></button>
@@ -59,8 +69,8 @@ const BombFarms = () => {
                 <div className="boardroomHeadings" style={{width:"550px"}}>
                     <p className="headlines">BSHARE-BNB <span className="recommended">Recommended</span></p>
                     <div style={{display: "flex", justifyContent:"space-between", flexDirection:"row"}}>
-                        <p className="headlines" style={{fontSize: "smaller"}}>Stake BSHARE and earn BOMB every epoch</p>
-                        <p style={{color:"white"}}>TVL: ${'tvl.toFixed(2)'}</p>
+
+                        <p style={{color:"white"}}>TVL: ${tvl.toFixed(2)}</p>
                     </div>
                 </div>
             </div>
@@ -86,7 +96,7 @@ const BombFarms = () => {
                     <div>
                         <button className="button boardroomDeposit" >Deposit <img style={{height:"18px", width:"18px", backgroundColor:"#6C9BCF", marginLeft:"3px"}} alt="img" src="https://img.icons8.com/?size=512&id=100000&format=png"/></button>
 
-                        <button className="button boardroomWithdraw" >Withdraw <img style={{height:"20px", width:"20px", backgroundColor:"#6C9BCF", marginLeft:"3px"}} alt="img" src="https://img.icons8.com/?size=512&id=102673&format=png"/></button>
+                        <button className="button boardroomWithdraw" onClick={handleWithdrawBomb}>Withdraw <img style={{height:"20px", width:"20px", backgroundColor:"#6C9BCF", marginLeft:"3px"}} alt="img" src="https://img.icons8.com/?size=512&id=102673&format=png"/></button>
 
                     </div>
                     <button style={{display: "flex", justifyContent: "center", alignItems:"center"}} className="button boardroomClaim" >Claim Rewards <img style={{height:"18px", width:"18px"}} alt="bombimg"  src="https://s3-alpha-sig.figma.com/img/a4ce/8dfa/11770664532158db85f355f7654b42d7?Expires=1684108800&Signature=CIlhPZwuugGXFhdDHFxthynQV6kuCT9o3J9oOSBtBnEFBsVx7dlARHPg9GCRnYniOUwEbORcXUPG02g6mb4Z-LZc8R1POR9lbVgdNnxj3~o~9~80CvFAf6eF6TbnBKT-RqTkl8Iu8a5RPfKcIgIjfDTFACUlphyo-Eq1tX-AZX26RPS9eFClnrRvSZZry97DctNCN~nyu6~8ZtXlgc1WuEPmKF9M0fE5XrciIUNMDDAfKvZ2tKOtOkLpcyXS~jXRJJOEnKtCmA34y1MEhnuF3EHz4PNFhNGZBybNmc4NLlvO-PbratXyv1Qd9VmlnY0E1f3p5u1Zd712XXgZeCPYAw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"  /></button>
